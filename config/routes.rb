e208397e1fab93cfb8183220713c4056d1810d76
Caitlyn Sun <caitlyn.y.sun@gmail.com>
2021-08-21 22:04:01 -0400

    Version 3 - Signup form set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home', as: :home
   get '/about' => 'pages#about', as: :about
+  resources :signups
+  get '/new' => 'signups#new', as: :sign
+  post 'signups' => 'signups#create'
+  get '/thanks' => 'pages#thanks', as: :thanks
 end
