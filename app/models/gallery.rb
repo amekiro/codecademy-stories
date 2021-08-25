9e00d0ce352714d5faa9be578a1f9e22d3c7b8fa
Caitlyn Sun <caitlyn.y.sun@gmail.com>
2021-08-24 20:32:48 -0400

    Version 7 - Set up gallery navigation

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,9 @@
 class Gallery < ApplicationRecord
+    def self.get_previous_img(img)
+        Gallery.where("galleries.id < ? ", img.id).last
+    end
+    
+    def self.get_next_img(img)
+        Gallery.where("galleries.id > ? ", img.id).first
+    end
 end
